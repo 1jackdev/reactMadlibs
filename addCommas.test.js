788d9f419bb110c73bf64ab48cc92d963eef0cf0
Jack <jackmayer21@gmail.com>
2021-04-24 10:41:48 -0700

    add tests

--- addCommas.test.js
+++ addCommas.test.js
@@ -4,4 +4,16 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+
+test('should handle minus sign', () => {
+  const result = addCommas(-332)
+  expect(result[0]).toEqual("-")
+})
+
+test('should add commas when length > 3', () => {
+  const result = addCommas(1245)
+  expect(result).toContain(",")
+});
+
+
 });
