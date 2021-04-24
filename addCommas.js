9fe6de4982b51994acb993db6f29e78ec7e33ff4
Jack <jackmayer21@gmail.com>
2021-04-24 10:34:50 -0700

    add commas func

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,20 @@
-function addCommas() {}
+function addCommas(num) {
+  let minusSign = "";
+  const numArr = num.toString().split("");
+  if (numArr[0] === "-") {
+    minusSign = numArr.splice(0, 1);
+  }
+  const prettyNumArr = [];
+  revNumArr = numArr.reverse();
+  for (let i = 0; i <= revNumArr.length - 1; i++) {
+    if (i !== 0 && i % 3 === 0) {
+      prettyNumArr.push(",");
+    }
+    prettyNumArr.push(revNumArr[i]);
+  }
+
+  const prettyNumStr = prettyNumArr.reverse().join("");
+  return minusSign + prettyNumStr;
+}
 
 module.exports = addCommas;
